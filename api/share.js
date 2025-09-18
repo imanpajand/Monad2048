6969c5b948cb455c946de740766e5a6355a8c833
ImanPJN <imanpajand@gmail.com>
2025-09-18 12:50:09 +0400

    Update share.js

--- api/share.js
+++ api/share.js
@@ -1,7 +1,7 @@
 
 export default function handler(req, res) {
   const text = encodeURIComponent(
-    "Play fun and addictive #2048 game on Arbitrum and save your score on-chain on top the leaderboard! 🔵 🎮\nArbitrum 2048 #MiniApp by @imaan 💜 \nhttps://arb2048.vercel.app"
+    "Play fun and addictive #2048 game on Monad Testnet and save your score on-chain on top the leaderboard! 🟣 🎮\nArbitrum 2048 #MiniApp by @imaan 💜 \nhttps://monadx2048.vercel.app"
   );
   const url = `https://farcaster.xyz/~/compose?text=${text}`;
   res.writeHead(302, { Location: url });
